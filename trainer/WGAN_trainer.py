76be04c93a520e4ec6fef705d0925374e214adb1
Nguyen Thai Huu Huy <81891365+huynth1801@users.noreply.github.com>
2022-02-19 21:36:10 +0700

    fixed trainer

--- trainer/WGAN_trainer.py
+++ trainer/WGAN_trainer.py
@@ -90,83 +90,87 @@ def WGANTrainer(opt):
     discriminator = discriminator.cuda()
     perceptualnet = perceptualnet.cuda()
 
-        # Define the train set
-        trainset = InpaintDataset(opt)
-        # Define dataloader
-        dataloader = DataLoader(trainset, batch_size = opt.batch_size, shuffle = True, 
-                            num_workers = opt.num_workers, pin_memory = True)
+    # Define the train set
+    trainset = InpaintDataset(opt)
+    # Define dataloader
+    dataloader = DataLoader(trainset, batch_size = opt.batch_size, shuffle = True, 
+                        num_workers = opt.num_workers, pin_memory = True)
 
-        # Initialize start time
-        start_time = time.time()
+    # Initialize start time
+    start_time = time.time()
+
+    # Create Tensor cuda
+    Tensor = torch.cuda.FloatTensor
 
         # training loop
-        for epoch in range(opt.epochs):
-            for batch_idx, (img, mask) in enumerate(dataloader):
-                # Load mask (shape: [B, 1, H, W]), masked_img (shape: [B, 3, H, W]), 
-                # img (shape: [B, 3, H, W]) and put it to cuda
-                img = img.cuda()
-                mask = mask.cuda()
+    for epoch in range(opt.resume_epochs, opt.epochs):
+        for batch_idx, (img, mask) in enumerate(dataloader):
+            # Load mask (shape: [B, 1, H, W]), masked_img (shape: [B, 3, H, W]), 
+            # img (shape: [B, 3, H, W]) and put it to cuda
+            img = img.cuda()
+            mask = mask.cuda()
 
-                # Train discriminator
-                optimizer_d.zero_grad()
+            # Train discriminator
+            optimizer_d.zero_grad()
 
-                # Generator ouput
-                first_out, second_out = self.G(img, mask)
+            # Generator ouput
+            first_out, second_out = self.G(img, mask)
 
-                # Forward propagation
-                first_out_whole_img = img * (1 - mask) + first_out * mask        # in range [0, 1]
-                second_out_whole_img = img * (1 - mask) + second_out * mask      # in range [0, 1]
+            # Forward propagation
+            first_out_whole_img = img * (1 - mask) + first_out * mask        # in range [0, 1]
+            second_out_whole_img = img * (1 - mask) + second_out * mask      # in range [0, 1]
 
-                # Fake samples
-                fake_scalar = self.D(second_out_whole_img.detach(), mask)
-                # True samples
-                true_scalar = self.D(img, mask)
+            # Fake samples
+            fake_scalar = self.D(second_out_whole_img.detach(), mask)
+            # True samples
+            true_scalar = self.D(img, mask)
 
-                # Overall Loss and optimize
-                loss_D = torch.mean(fake_scalar) - torch.mean(true_scalar)
-                loss_D.backward()
-                optimizer_d.step()
+            # Overall Loss and optimize
+            loss_D = torch.mean(fake_scalar) - torch.mean(true_scalar)
+            loss_D.backward()
+            optimizer_d.step()
 
-                # Train generator
-                optimizer_g.zero_grad()
+            # Train generator
+            optimizer_g.zero_grad()
 
-                # Mask L1 Loss
-                first_MaskL1Loss = L1Loss(first_out_whole_img, img)
-                second_MaskL1Loss = L1Loss(second_out_whole_img, img)
+            # Mask L1 Loss
+            first_MaskL1Loss = L1_loss(first_out_whole_img, img)
+            second_MaskL1Loss = L1_loss(second_out_whole_img, img)
 
-                # GAN Loss
-                fake_scalar = self.D(second_out_whole_img, mask)
-                GAN_Loss = - torch.mean(fake_scalar)
+            # GAN Loss
+            fake_scalar = discriminator(second_out_wholeimg, mask)
+            GAN_Loss = -torch.mean(fake_scalar)
 
-                # Get the deep semantic feature maps, and compute Perceptual Loss
-                img_featuremaps = self.percep_net(img)                            # feature maps
-                second_out_wholeimg_featuremaps = self.percep_net(second_out_whole_img)
-                second_PerceptualLoss = L1Loss(second_out_wholeimg_featuremaps, img_featuremaps)
+            # Get the deep semantic feature maps, and compute Perceptual Loss
+            img_featuremaps = perceptualnet(img)                            # feature maps
+            second_out_wholeimg_featuremaps =perceptualnet(second_out_whole_img)
+            second_PerceptualLoss = L1_loss(second_out_wholeimg_featuremaps, img_featuremaps)
 
-                # Compute losses
-                loss = opt.lambda_l1 * first_MaskL1Loss + opt.lambda_l1 * second_MaskL1Loss + \
-                    opt.lambda_perceptual * second_PerceptualLoss + opt.lambda_gan * GAN_Loss
-                loss.backward()
-                optimizer_g.step()
+            # Compute losses
+            loss = opt.lambda_l1 * first_MaskL1Loss + opt.lambda_l1 * second_MaskL1Loss + \
+                        opt.lambda_perceptual * second_PerceptualLoss + opt.lambda_gan * GAN_Loss
+            loss.backward()
+            optimizer_g.step()
 
-                # Determine approximate time left
-                batches_done = epoch * len(dataloader) + batch_idx
-                batches_left = opt.epochs * len(dataloader) - batches_done
-                time_left = datetime.timedelta(seconds = batches_left * (time.time() - start_time))
-                start_time = time.time()
+            # Determine approximate time left
+            batches_done = epoch * len(dataloader) + batch_idx
+            batches_left = opt.epochs * len(dataloader) - batches_done
+            time_left = datetime.timedelta(seconds = batches_left * (time.time() - start_time))
+            start_time = time.time()
 
-                # Print log
-                print("\r[Epoch {}/{}] [Batch {}/{}] [First Mask L1 Loss: {:.4f}] [Second Mask L1 Loss: {:.4f}]".format(
-                    ((epoch + 1), opt.epochs, batch_idx, len(dataloader), first_MaskL1Loss.item(), second_MaskL1Loss.item())))
-                print("\r[D Loss: %.5f] [G Loss: %.5f] [Perceptual Loss: %.5f] time_left: %s" %
-                    (loss_D.item(), GAN_Loss.item(), second_PerceptualLoss.item(), time_left))
-                print('-'*30)
-            # Learning rate decrease
-        self.adjust_learning_rate(opt.lr_g, optimizer_g, (epoch + 1), opt)
-        self.adjust_learning_rate(opt.lr_d, optimizer_d, (epoch + 1), opt)
+            # Print log
+            print("\r[Epoch {}/{}] [Batch {}/{}] [First Mask L1 Loss: {:.4f}] [Second Mask L1 Loss: {:.4f}]".format(
+                ((epoch + 1), opt.epochs, batch_idx, len(dataloader), first_MaskL1Loss.item(), second_MaskL1Loss.item())))
+            print("\r[D Loss: %.5f] [G Loss: %.5f] [Perceptual Loss: %.5f] time_left: %s" %
+                (loss_D.item(), GAN_Loss.item(), second_PerceptualLoss.item(), time_left))
+            print('-'*50)
+        # Learning rate decrease
+        adjust_learning_rate(opt.lr_g, optimizer_g, (epoch + 1), opt)
+        adjust_learning_rate(opt.lr_d, optimizer_d, (epoch + 1), opt)
 
         # Save the model
-        self.save((epoch + 1))
+        save_model_generator(generator, (epoch + 1), opt)
+        save_model_discriminator(discriminator, (epoch + 1), opt)
 
         ### Sample data every epoch
         masked_img = img * (1 - mask) + mask
