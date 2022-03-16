7bb6594f9fe036f596721046f7009ebbb1ac518e
huynth1801 <huuhuy1801@gmail.com>
2022-03-16 10:20:05 +0700

    fixed templates

--- dataset/train_dataset.py
+++ dataset/train_dataset.py
@@ -21,9 +21,9 @@ class InpaintDataset(Dataset):
             for filepaths in files:
                 self.img_list.append(os.path.join(root, filepaths))
 
-        # Nếu dataset là Places2"
-        for img in glob.glob(os.path.join(opt.baseroot, "**/**/extra_train_*.jpg")):
-            self.img_list.append(img)
+        # # Nếu dataset là Places2"
+        # for img in glob.glob(os.path.join(opt.baseroot, "**/**/extra_train_*.jpg")):
+        #     self.img_list.append(img)
     
     def __len__(self):
         return len(self.img_list)
