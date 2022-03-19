6950e87f54358899651da2bb1b54a49afb727dd7
huynth1801 <huuhuy1801@gmail.com>
2022-03-19 17:13:23 +0700

    add app for image inpainting

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import base64
+from crypt import methods
+from fileinput import filename
 import os
 from tkinter.tix import IMAGE, Tree
 import cv2
@@ -6,6 +9,7 @@ import numpy as np
 import json
 import uuid
 from PIL import Image
+from predict import predict
 
 
 UPLOAD_FOLDER  = 'static/uploads'
@@ -27,8 +31,42 @@ def resize(img_path, size=(128,128)):
     img = img.resize(img, Image.ANTIALIAS)
     img.save(img_path)
     
+@app.route('/process', methods=["POST"])
+def process():
+    try:
+        filename = str(uuid.uuid4())
+        file_path_raw = os.path.join(app.config["UPLOAD_FOLDER"], filename + '.png')
+        file_path_mask = os.path.join(app.config["UPLOAD_FOLDER"], 'mask_' + filename + '.png')
+        file_path_output = os.path.join(app.config["UPLOAD_FOLDER"], 'output_' + filename + '.png')
 
+        # Save mask
+        mask_b64 = request.values[('mask_b64')]
+        img_str = mask_b64.split(',')[1]
+        output = open(file_path_mask, 'wb')
+        decoded = base64.b64decode(img_str)
+        output.write(decoded)
+        output.close()
+        resize(file_path_mask)
 
+        # Save raw image
+        file_raw = request.files.get('input_file')
+        file_raw.save(file_path_raw)
+        resize(file_path_raw)
+
+        # Doing inpainting
+        output = predict(file_path_raw, file_path_mask)
+        cv2.imwrite(file_path_output, output)
+        resize(file_path_output, size=(256,256))
+
+        return jsonify(
+            {
+                'output_image': os.path.join('static', 'uploads', os.path.basename(file_path_output))
+            }
+        )
+
+    except Exception as error:
+        print(error)
+        return jsonify({'status': 'error'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000, use_reloader=True, threaded=False)
---
+++ predict.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+from options.test_options import TestOptions
+from utils.utils import create_generator
+from PIL import Image
+
+# Options
+opt = TestOptions().parse()
+
+def load_model(generator, epoch, opt):
+    pre_dict = torch.load(opt.load_name)
+    generator.load_state_dict(pre_dict)
+
+    if not os.path.exists(opt.results_path):
+        os.makedirs(opt.results_path)
+
+# Load moel generator
+model = create_generator(opt).eval()
+load_model(model, opt.epochs, opt)
+print("Load pretrained model generator")
+
+
+def predict(img, mask, opt=None):
+    img = img.convert('RGB')
+    img_raw = np.array(img)
+    w_raw, h_raw = img.size
+    h_t, w_t = h_raw // 8*8, w_raw // 8*8
+
+    img = img.resize((w_t, h_t))
+    img = np.array(img).transpose(2,0,1)
+
+    mask_raw = np.array(mask)[..., None] > 0
+    mask = mask.resize((w_t, h_t))
+
+    mask = np.array(mask)
+    mask = (torch.Tensor(mask) > 0).float()
+    img = (torch.Tensor(img)).float()
+
+    img = (img / 255 - 0.5) / 0.5
+    img = img[None]
+    mask = mask[None, None]
+
+    with torch.no_grad():
+        generated, _ = model(img , mask)
+    
+    generated = torch.clamp(generated, -1, 1)
+    generated = (generated + 1) / 2 * 255
+    generated = generated.cpu().nupy().astype(np.uint8)
+    generated = generated[0].transpose((1, 2, 0))
+    result = generated * mask_raw + img_raw * (1 - mask_raw)
+    result = result.astype(np.unit8)
+
+    # result = Image.fromarray(result).resize((w_raw, h_raw))
+    # result = np.array(result)
+    # result = Image.fromarray(result.astype(np.uint8))
+    # result.save(f'static/results/{name}')
